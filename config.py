07d1bb0fcc45312b8eb2e51e71a1c4111fa93fff
Luis Diego <yeye.rodriguez.27@gmail.com>
2024-11-09 21:17:30 -0300

    debug

--- config.py
+++ config.py
@@ -9,3 +9,8 @@ class Config:
     POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
     POSTGRES_DB = os.getenv('POSTGRES_DB')
     secret_key = os.getenv('SECRET_KEY')
+    
+    def __init__(self):
+        print(f"POSTGRES_HOST: {self.POSTGRES_HOST}")
+        print(f"POSTGRES_USER: {self.POSTGRES_USER}")
+        print(f"POSTGRES_DB: {self.POSTGRES_DB}")
