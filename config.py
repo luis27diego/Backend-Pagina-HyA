11a034bbc33ccfb00bd118529e59bb6dc679d313
Luis Diego <yeye.rodriguez.27@gmail.com>
2024-11-09 21:32:42 -0300

    test

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()  # Carga las variables de entorno desde .env
 
 class Config:
-    POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+    POSTGRES_HOST = 'postgres.railway.internal'
     POSTGRES_USER = os.getenv('POSTGRES_USER')
     POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
     POSTGRES_DB = os.getenv('POSTGRES_DB')
     secret_key = os.getenv('SECRET_KEY')
-    
+
     def __init__(self):
         print(f"POSTGRES_HOST: {self.POSTGRES_HOST}")
         print(f"POSTGRES_USER: {self.POSTGRES_USER}")
